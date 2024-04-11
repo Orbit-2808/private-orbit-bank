20a9c96bb1eec3136f5ae55f95e1b4a3c77cf826
FITurnip <franklinirtr@gmail.com>
2024-04-12 00:45:35 +0700

    auth module

---
+++ controller/auth.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Auth adalah fitur web.
+ * Cukup gunakan apabila diperlukan saja
+ * Hindari pengubahan pada fitur ini demi keamanan data
+ */
+enum SessionCondtion {
+    case loggedIn;
+    case unLoggedIn;
+    case wrong;
+};
+
+session_start();
+
+function isLoggedIn() {
+    if (isset($_SESSION["condition"])) $sessionCondition = $_SESSION["condition"];
+    else $sessionCondition = SessionCondtion::unLoggedIn;
+    return $sessionCondition;
+}
+
+function login($username, $password) {
+    if ($_SESSION["condition"] != SessionCondtion::loggedIn) {
+        include_once($_SERVER['DOCUMENT_ROOT'] . "/database/database.php");
+        $conn = db_connect();
+    
+        $query = "SELECT * FROM users WHERE username = '$username' AND password = SHA2('$password', 224)";
+    
+        $result = mysqli_query($conn, $query);
+    
+        if (mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION["username"] = $row["username"];
+            $_SESSION["email"] = $row["email"];
+            $_SESSION["condition"] = SessionCondtion::loggedIn;
+        } else {
+            $_SESSION["condition"] = SessionCondtion::wrong;
+            session_destroy();
+        }
+    
+        mysqli_close($conn);
+    } else {
+        $_SESSION["condition"] = SessionCondtion::wrong;
+        session_destroy();
+    }
+
+    return $_SESSION["condition"];
+}
+
+function logout() {
+    session_destroy();
+    session_start();
+    $_SESSION["condition"] = SessionCondtion::unLoggedIn;
+}
+
+function register($username, $email, $password) {
+    if ($_SESSION["condition"] != SessionCondtion::loggedIn) {
+        include_once($_SERVER['DOCUMENT_ROOT'] . "/database/database.php");
+        $conn = db_connect();
+    
+        $query = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', SHA2('$password', 224))";
+        $result = mysqli_query($conn, $query);
+    }
+    $sessionCondition = login($username, $password);
+    return $sessionCondition;
+}
