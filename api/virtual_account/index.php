29cc5e06132828d77f33191835631adb4697a3b5
FITurnip <franklinirtr@gmail.com>
2024-04-13 02:43:32 +0700

    add edit data

--- api/virtual_account/index.php
+++ api/virtual_account/index.php
@@ -16,8 +16,9 @@ switch ($_SERVER["REQUEST_METHOD"]) {
     /**
      * 
      * make virtual account
-     * request  : account_id, amount
-     * response :
+     * request  : account_id, amount, information
+     * response : virtual_account_number, amount, creation_datetime, expired_date
+     * 
      */
     case "POST":
         $jsonData = file_get_contents('php://input');
@@ -26,12 +27,12 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         $jsonDataDecode = json_decode($jsonData, true);
 
         // get data from json data decode
-        $receiveAccountId = $jsonDataDecode["receive_account_id"];
-        $amount = $jsonDataDecode["amount"];
-        $information = $jsonDataDecode["information"];
-        
+        $request["receiver_account_number"] = $jsonDataDecode["receiver_account_number"];
+        $request["amount"] = $jsonDataDecode["amount"];
+        $request["information"] = $jsonDataDecode["information"];
+
         // make virtual akun
-        $dataTemp = createVirtualAccount($receiveAccountId, $amount, $information);
+        $dataTemp = createVirtualAccount($request);
 
         // dont expose all data
         $data = [
@@ -47,14 +48,14 @@ switch ($_SERVER["REQUEST_METHOD"]) {
      * 
      * get virtual account data
      * request  : virtual_account_number
-     * response : 
+     * response : receiver_account_id, amount, information, creation_datetime, expired_date, transaction_conditon
      * 
      */
     case "GET":
-        $virtualAccountNumber = $_GET["virtual_account_number"];
-        if (empty($virtualAccountNumber) == false) {
+        $request["virtual_account_number"] = $_GET["virtual_account_number"];
+        if (empty($request["virtual_account_number"]) == false) {
             // get data
-            $data = getVirtualAccountData($virtualAccountNumber);
+            $data = getVirtualAccountData($request);
         } else {
             // send error data
             $data = array(
@@ -68,12 +69,20 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
     /**
      * 
-     * edit virtual account data
+     * edit virtual account data: only use this for cancel virtual account
      * request  : account_number
      * response : 
      * 
      */
     case "PUT":
+        $jsonData = file_get_contents('php://input');
+    
+        // decode JSON data
+        $jsonDataDecode = json_decode($jsonData, true);
+        $request = $jsonDataDecode;
+        $virtualAccount = $_GET;
+
+        $data = editVirtualAccount($request, $virtualAccount);
         break;
 }
 
--- module/virtual_account.php
+++ module/virtual_account.php
@@ -12,32 +12,31 @@ function generateVirtualAccountNumber($receiverAccountNumber) {
     return $virtualAccountNumber;
 }
 
-function getVirtualAccountData($virtualAccountNumber) {
+function getVirtualAccountData($request) {
     $conn = db_connect();
-    $sql = "SELECT receiver_account_id, amount, information, creation_datetime, expired_date, transaction_conditon
+    $sql = "SELECT virtual_account_id, receiver_account_id, amount, information, creation_datetime, expired_date, transaction_conditon
             FROM virtual_accounts
-            WHERE virtual_account_number = '$virtualAccountNumber'";
+            WHERE virtual_account_number = '{$request["virtual_account_number"]}'";
     $result = mysqli_query($conn, $sql);
     $data = mysqli_fetch_assoc($result);
     mysqli_close($conn);
     return $data;
 }
 
-function createVirtualAccount($receiverAccountNumber, $amount, $information) {
+function createVirtualAccount($request) {
     $conn = db_connect();
 
     // get data needed
-    $account_id = _getAccountId($receiverAccountNumber, $conn);
-    $virtualAccountNumber = generateVirtualAccountNumber($receiverAccountNumber);
-
+    $account_id = _getAccountId($request["receiver_account_number"], $conn);
+    $virtualAccountNumber = generateVirtualAccountNumber($request["receiver_account_number"]);
+    
     // insert data
     $sql = "INSERT INTO `virtual_accounts`
             (`receiver_account_id`, `virtual_account_number`, `amount`, `information`, `creation_datetime`, `expired_date`, `transaction_conditon`)
             VALUES
-            ($account_id, '$virtualAccountNumber', $amount, '$information', current_timestamp(), ADDDATE(current_timestamp(), INTERVAL 1 DAY), 'waiting for transaction')";
-
+            ($account_id, '$virtualAccountNumber', {$request["amount"]}, '{$request["information"]}', current_timestamp(), ADDDATE(current_timestamp(), INTERVAL 1 DAY), 'waiting for transaction')";
     $result = mysqli_query($conn, $sql);
-
+    
     $virtualAccountId = mysqli_insert_id($conn);
 
     mysqli_close($conn);
@@ -48,4 +47,47 @@ function createVirtualAccount($receiverAccountNumber, $amount, $information) {
 
     // don't return all data
     return $data;
+}
+
+function editVirtualAccount($request, $virtualAccount) {
+    $conn = db_connect();
+
+    $virtualAccountData = getVirtualAccountData($virtualAccount);
+    
+    if(empty($request["receiver_account_number"]) == false)
+        $request["receiver_account_id"] = _getAccountId($request["receiver_account_number"], $conn);
+
+
+    $updates = [
+        'receiver_account_id' => $request["receiver_account_id"],
+        'virtual_account_number' => $request["virtual_account_number"],
+        'amount' => $request["amount"],
+        'information' => $request["information"],
+        'expired_date' => $request["expired_date"]
+    ];
+    
+    // Loop through the updates array and construct the SET clause
+    foreach ($updates as $column => $value) {
+        if ($value !== null) {
+            $setClauses[] = "`$column` = '$value'";
+        }
+    }
+    
+    // Join the SET clause elements with commas
+    $setClause = implode(", ", $setClauses);
+
+    $sql = "UPDATE `virtual_accounts`
+        SET
+        $setClause
+        WHERE
+        virtual_account_id = {$virtualAccountData["virtual_account_id"]}";
+
+    // return $sql;
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
+
+    // get data
+    $virtualAccountData = getVirtualAccountData($virtualAccount);
+
+    return $virtualAccountData;
 }
