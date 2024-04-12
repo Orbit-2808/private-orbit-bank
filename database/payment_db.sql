fcc5874c6166c5f40686743fe6e1cb864ac5f17e
FITurnip <franklinirtr@gmail.com>
2024-04-12 14:27:34 +0700

    add create and delete db

--- database/payment_db.sql
+++ database/payment_db.sql
@@ -21,6 +21,10 @@ SET time_zone = "+00:00";
 -- Database: `payment_db`
 --
 
+DROP DATABASE IF EXISTS payment_db;
+CREATE DATABASE payment_db;
+USE payment_db;
+
 -- --------------------------------------------------------
 
 --
